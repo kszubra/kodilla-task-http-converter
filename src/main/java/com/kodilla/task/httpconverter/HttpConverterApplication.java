7c71857ac0e44133fd7e420c8026091d5b79e9bf
Kacper Szubra <kacper.szubra@nvt.pl>
2021-10-18 18:07:47 +0200

    initial commit

---
+++ src/main/java/com/kodilla/task/httpconverter/HttpConverterApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.task.httpconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpConverterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/task/httpconverter/HttpConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.task.httpconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
